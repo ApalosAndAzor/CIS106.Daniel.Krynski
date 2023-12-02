a8941db6d580aaf432471dbfaaf5b6cd7903b43a
ApalosAndAzor <98003711+ApalosAndAzor@users.noreply.github.com>
2023-12-01 22:50:42 -0600

    Add files via upload

---
+++ PS12/PS12P1.py
@@ -0,0 +1,332 @@
+#problem 1
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+#problem 2
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+#problem 3
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+#problem 4
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+#problem 5
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+list_one.remove(800)
+print(list_one)
+#problem 6
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+list_one.remove(800)
+print(list_one)
+list_one.pop(2)
+print(list_one)
+#problem 7
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+list_one.remove(800)
+print(list_one)
+list_one.pop(2)
+print(list_one)
+grades = ["A" , "B" , "C" , "A" , "A", "C"]
+#problem 8
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+list_one.remove(800)
+print(list_one)
+list_one.pop(2)
+print(list_one)
+grades = ["A" , "B" , "C" , "A" , "A", "C"]
+print(grades.count("C"))
+#problem 9
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+list_one.remove(800)
+print(list_one)
+list_one.pop(2)
+print(list_one)
+grades = ["A" , "B" , "C" , "A" , "A", "C"]
+print(grades.count("C"))
+print(grades.index("B"))
+#problem 10
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+list_one.remove(800)
+print(list_one)
+list_one.pop(2)
+print(list_one)
+grades = ["A" , "B" , "C" , "A" , "A", "C"]
+print(grades.count("C"))
+print(grades.index("B"))
+look_for = "F"
+if look_for in grades:
+  print(look_for, "is in the list")
+else:
+  print(look_for, "is not in the list")
+#problem 11
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+list_one.remove(800)
+print(list_one)
+list_one.pop(2)
+print(list_one)
+grades = ["A" , "B" , "C" , "A" , "A", "C"]
+print(grades.count("C"))
+print(grades.index("B"))
+look_for = "F"
+if look_for in grades:
+  print(look_for, "is in the list")
+else:
+  print(look_for, "is not in the list")
+list_two.clear()
+print(list_two)
+#problem 12
+def define(list_one):
+  x = int(input("How many numbers are in the list?"))
+  for x in range(0, x, 1):
+    y = int(input("Enter the numbers:"))
+    list_one.append(y)
+  return list_one
+def display(list_one):
+  for z in list_one:
+    print(z)
+list_one = []
+list_one = define(list_one)
+display(list_one)
+print(list_one)
+list_one.insert(1, 99)
+print(list_one)
+list_one[1] = 100
+print(list_one)
+list_two = [500, 600, 700, 800, 900]
+print(list_two)
+list_one.extend(list_two)
+print(list_one)
+list_one.remove(800)
+print(list_one)
+list_one.pop(2)
+print(list_one)
+grades = ["A" , "B" , "C" , "A" , "A", "C"]
+print(grades.count("C"))
+print(grades.index("B"))
+look_for = "F"
+if look_for in grades:
+  print(look_for, "is in the list")
+else:
+  print(look_for, "is not in the list")
+list_two.clear()
+print(list_two)
+del list_two
+print(list_two)
+#problem 13
+players = ["Rizzo", "Davis", "Baez", "Happ", "Bryan"]
+#problem 14
+players = ["Rizzo", "Davis", "Baez", "Happ", "Bryan"]
+players.sort()
+print(players)
+#problem 15
+players = ["Rizzo", "Davis", "Baez", "Happ", "Bryan"]
+players.sort()
+print(players)
+players2 = players.copy()
+print(players2)
+#problem 16
+players = ["Rizzo", "Davis", "Baez", "Happ", "Bryan"]
+players.sort()
+print(players)
+players2 = players.copy()
+print(players2)
+players2.reverse()
+print(players2)
